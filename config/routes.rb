10fa9fa1215f4e596ddf1ad785ccd98f3a7bdcbe
William <willspecker@gmail.com>
2020-11-17 21:22:47 +0100

    writing test to see if entry will be saved to database creating performance data controller creating create performance data route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
 
-    namespace :v1 do  
+    namespace :v1 do
+      resources :performance_data, only: [:create]  
     end
   end
 end
